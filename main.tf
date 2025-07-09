13ee6c17264f0bc469b0b67bfe573e23b0f707a0
Vinith Amma <vinithamma007@gmail.com>
2025-07-09 21:01:42 +0530

    app module autoscalling group

--- main.tf
+++ main.tf
@@ -31,10 +31,10 @@ resource "aws_launch_template" "main" {
   image_id      = data.aws_ami.ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
-  user_data = filebase64(templatefile("${path.module}/userdata.sh"),
+  user_data = base64encode(templatefile("${path.module}/userdata.sh",
     {
       component = var.component
-  })
+  }))
 
   tag_specifications {
     resource_type = "instance"
@@ -42,14 +42,14 @@ resource "aws_launch_template" "main" {
   }
 }
 
-resource "aws_autoscaling_group" "main" {
-  availability_zones = ["us-east-1a"]
-  desired_capacity   = 1
-  max_size           = 1
-  min_size           = 1
-
-  launch_template {
-    id      = aws_launch_template.main.id
-    version = "$Latest"
-  }
-}
+#resource "aws_autoscaling_group" "main" {
+#  availability_zones = ["us-east-1a"]
+#  desired_capacity   = 1
+#  max_size           = 1
+#  min_size           = 1
+#
+#  launch_template {
+#    id      = aws_launch_template.main.id
+#    version = "$Latest"
+#  }
+#}
