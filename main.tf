a573165b22a1f47bdc88912f4765dbf4e48474f9
Vinith Amma <vinithamma007@gmail.com>
2025-07-21 20:29:54 +0530

    app module alb&tg s-42

--- main.tf
+++ main.tf
@@ -99,8 +99,8 @@ resource "aws_lb_target_group" "public" {
   vpc_id   = var.vpc_id
 }
 resource "aws_lb_target_group_attachment" "public" {
-  count = data.dns_a_record_set.private_alb_records.addrs
+  count = tolist(data.dns_a_record_set.private_alb_records.addrs)
   target_group_arn = aws_lb_target_group.public[0].arn
-  target_id        = element(data.dns_a_record_set.private_alb_records.addrs, count.index )
+  target_id        = element(tolist(data.dns_a_record_set.private_alb_records.addrs), count.index )
   port             = 80
 }
