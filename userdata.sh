5bd92015dd35c3bb9b0f719469510f66892022db
Vinith Amma <vinithamma007@gmail.com>
2025-07-25 20:00:10 +0530

    s-43

--- userdata.sh
+++ userdata.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-yum install ansible python3.11-pip.noarch -y   &>>/opt/userdata.log
+yum install ansible python3.12-pip.noarch -y   &>>/opt/userdata.log  #you can use here v-3.12 as well
 
-pip3.11 install botocore boto3 &>>/opt/userdata.log
+pip3.12 install botocore boto3 &>>/opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/vinithrepo/roboshop-ansible.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
 
 
